f71e3001b5dc503e08b2bebfdc84b126f2b0c88e
minhphuoc275 <35521459+minhphuoc275@users.noreply.github.com>
2019-05-02 16:49:54 +0700

    Update 01_the_first_nodejs.js
    Edited this file to test git pull function

--- 01_the_first_nodejs.js
+++ 01_the_first_nodejs.js
@@ -1,1 +1,2 @@
-console.log("Hello Nodejs! This is first time");
+console.log("Hello Nodejs! This is first time! Just simple run command: node 01_the_first_nodejs.js to execute this program!");
+console.log("Edit to test git pull");
