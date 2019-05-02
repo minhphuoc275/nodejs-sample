f1185c315b89792f11b003b70d23a7e09ffb1099
Phuoc Tran <minhphuoc275@gmail.com>
2019-05-02 16:25:17 +0700

    First commit for 03_IfForWhile.js

---
+++ 03_IfForWhile.js
@@ -0,0 +1,17 @@
+var iheight = 165;
+
+if ( iheight >= 165 ) {
+	console.log("With " + iheight + ", you have a good height");
+} else {
+	console.log("Need to do exercise to get higher");
+}
+
+for(var i=1; i<=10; i++){
+	console.log("Number in for loop: " + i);
+}
+
+var irow=1;
+while(irow<=5){
+	console.log("Line number in while: " + irow);
+	irow = irow + 1
+}
